29ee743e2699ff61ea2d78f7e5e9604a404edc10
kusmin <kusmin00@hotmail.com>
2021-02-21 15:49:47 -0300

    inicio

--- teste/db/migrate/20210221174547_create_products.rb
+++ teste/db/migrate/20210221174547_create_products.rb
@@ -8,4 +8,18 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.timestamps
     end
   end
+
+  # def up
+  #   create_table :products do |t|
+  #     t.string :name, null:false
+  #     t.decimal :price
+  #     t.boolean :active
+
+  #     t.timestamps
+  #   end
+  # end
+
+  # def down
+  #   drop_table :products
+  # end
 end
